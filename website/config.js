161b8779bc55d7eaf2e565f6156319ccac265f86
bktrad <bktrad.service@gmail.com>
2019-01-06 20:52:15 +0100

    rename website_example -> website

---
+++ website/config.js
@@ -0,0 +1,13 @@
+var api = "https://pool.bktrad.com:8117";
+
+var email = "bktrad.service@gmail.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
